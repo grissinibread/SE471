31cfcbb3bd93ad0134ff170410f7777a72ca79ef
Alejandro Navarro <alejandronavarro@Alejandros-MacBook-Pro-4.local>
2025-02-07 09:27:45 -0800

    Skeleton of program created

---
+++ Lab1/Product.java
@@ -0,0 +1,32 @@
+public class Product {
+    // attributes
+    private final String id;
+    private String name;
+    private int price;
+
+    // Constructor
+    Product(String id) {
+        this.id = id;
+    }
+
+    // Methods
+    public String getID() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
---
+++ Lab1/SortingUtilityIF.java
@@ -0,0 +1,5 @@
+import java.util.List;
+
+public interface SortingUtilityIF {
+    public void sort(int sortingApproach, List<Product> items);
+}
