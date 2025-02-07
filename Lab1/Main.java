9d367f3fb031b201d7663441164ffbb5a9ed115a
Alejandro Navarro <alejandronavarro@Alejandros-MacBook-Pro-4.local>
2025-02-07 10:51:06 -0800

    Modified LoggingSortingUtility methods. Delted unnesecary test code in Main

--- Lab1/Main.java
+++ Lab1/Main.java
@@ -1,4 +1,36 @@
 package Lab1;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import java.util.List;
 
 public class Main {
+    public static void main(String args[]) {
+        List<Product> products = new ArrayList<>();
+        String productID;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many products would you like to log?");
+        int numOfProducts = scanner.nextInt();
+
+        for(int i = 0; i < numOfProducts; i++) {
+            scanner.nextLine(); // for junk contents
+
+            // product ID
+            System.out.println("Enter product id:");
+            productID = scanner.nextLine();
+
+            Product insert = new Product(productID);
+
+            // product name
+            System.out.println("Enter product name:");
+            insert.setName(scanner.nextLine());
+
+            // product price
+            System.out.println("Enter product price:");
+            insert.setPrice(scanner.nextInt());
+
+            products.add(insert);
+        }
+    }
 }
